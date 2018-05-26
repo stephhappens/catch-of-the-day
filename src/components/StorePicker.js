fb4cca818a6478d5fc4031a4dbdad20d5390eb78
Stephanie Provence <stephanie@lewiscommunications.com>
2018-05-25 19:21:36 -0500

    make starting files

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+// Get your ingredients
+import React from 'react';
+
+// Make your recipe
+class StorePicker extends React.Component {
+    render() {
+        return <p>hooooowdddy</p>
+    }
+}
+
+// Set it out to eat and share
+export default StorePicker;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Import your dependencies
+import React from 'react'; 
+import { render } from 'react-dom';
+
+// Import your components
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main'));
