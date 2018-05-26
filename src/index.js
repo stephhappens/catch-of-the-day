66ba395e760c8ac3bc1a67b81d84003ea071bc9f
Stephanie Provence <stephanie@lewiscommunications.com>
2018-05-25 21:07:59 -0500

    add react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // Import your dependencies
 import React from 'react'; 
 import { render } from 'react-dom';
-// Import your components
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+// Import your Router Component
+import Router from './components/Router';
 // Import Styles
 import "./css/style.css";
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
