51446488bbdaac8653aa70672e0fa31fc5a67b07
Stephanie Provence <stephanie@lewiscommunications.com>
2018-05-29 20:49:00 -0500

    add event binding and component state

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,30 @@
 // Get your ingredients
 import React from 'react';
+import { getFunName } from '../helpers';
 
 // Make your recipe
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {        
+        // Stop the form from submitting
+        event.preventDefault();
+        // get the text from the input
+        console.log(this.myInput);
+        // Change to page to /store/storename
+    }
     render() {
         return (
         <React.Fragment>
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" />
-                <button type="submit">Visit Store></button>
+                <input type="text"
+                ref="{this.myInput}"
+                required 
+                placeholder="Store Name" 
+                defaultValue={getFunName()}
+                />
+                <button type="submit" onClick={this.buttonClick}>Visit Store></button>
             </form> 
         </React.Fragment>
         )      
