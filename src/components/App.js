d26f62562de95ad368563a517f012c36f238c76f
Stephanie Provence <stephanie@lewiscommunications.com>
2018-06-08 17:59:07 -0500

    create fish component and update state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -18,7 +19,7 @@ class App extends React.Component {
     // Set the new fishes object to state
     this.setState({ fishes });
   };
-
+  // load fish data
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   }
@@ -27,6 +28,10 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" age={100} />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details=
+            {this.state.fishes[key]} />)}
+          </ul>
         </div>
         <Order />
         <Inventory 
